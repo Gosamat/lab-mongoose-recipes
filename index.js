a7cc16b3998852197d9e798e6e86b2e927833e69
Gosamat <61824985+Gosamat@users.noreply.github.com>
2023-08-03 16:44:57 +0100

    lab solved

--- index.js
+++ index.js
@@ -19,12 +19,50 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
-  } catch (error) {
+
+    // Iteration 2 - Create a recipe and Console.log the Title
+    let recipeOne = {
+      title: "choco frito", cuisine: "tuga"};
+    let recipeOneDb = await Recipe.create(recipeOne);
+    // console.log(recipeOneDb.title);
+    
+    // Iteration 3 - Insert multiple recipes and print the title of each recipe
+    let allRecipesDb = await Recipe.insertMany(data);
+
+    allRecipesDb.unshift(recipeOneDb); // Repo mentions Database now having 6 recipes so I unshifted it 🙃
+    allRecipesDb.forEach((element)=>{
+      // console.log(element.title)
+    });
+
+    // Iteration 4 - Update recipe
+    let rigatoni = { title: 'Rigatoni alla Genovese' };
+
+    let recipeUpdate = await Recipe.findOneAndUpdate(rigatoni, {duration: 100});
+    let findrigatoni = await Recipe.find({title: 'Rigatoni alla Genovese'});
+
+    console.log(findrigatoni);
+
+    // Iteration 5 - Remove a recipe
+
+    let carrotCake = { title: 'Carrot Cake' };
+    let deleteCarrotCake = await Recipe.deleteOne(carrotCake);
+    let updatedRecipeDb = await Recipe.find({});
+    console.log (updatedRecipeDb);
+
+    // Iteration 6 - Close the Database
+    const dbConnectionStop = mongoose.connection.close(MONGODB_URI);
+
+  } 
+  
+  catch (error) {
     console.log(error);
   }
 };
 
 manageRecipes();
+
+
+
 
 //Method 2: Using .then() method
 //If you want to use this method uncomment the code below:
@@ -42,3 +80,5 @@ manageRecipes();
   .catch((error) => {
     console.error('Error connecting to the database', error);
   }); */
+
+  
